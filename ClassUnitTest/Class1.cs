42c516222c350d14a1c41e5f1c72b9b4ca2feef5
Pooja Talaniya <pooja@FLEXI>
2024-02-13 13:03:39 -0500

    Add project files.

---
+++ ClassUnitTest/Class1.cs
@@ -0,0 +1,12 @@
+﻿using System;using System.Collections.Generic;using System.Linq;using System.Text;using System.Threading.Tasks;using NUnit.Framework;using NUnit.Framework.Legacy;using PoojaLab2;namespace ClassUnitTest{
+    // Pooja's Unit Test
+    public class TriangleTest1    {        public class Class1        {            [Test]            public void ValidTriangle_Input60and60and60_OutputValidTriangle()            {
+                // Pooja's Unit Test
+                //Arrange
+                int firstAngle = 60;                int secondAngle = 60;                int thirdAngle = 60;                string expected = "The triangle is valid.";
+
+                //Act
+                string actual = Triangle.ValidTriangle(firstAngle, secondAngle, thirdAngle);
+
+                //Assert
+                ClassicAssert.AreEqual(expected, actual);            }        }    }}
---
+++ PoojaLab2/Class1.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PoojaLab2
+{
+    // Pooja's Applications
+    public static class Triangle
+    {
+        public static string ValidTriangle(int firstAngle, int secondAngle, int
+        thirdAngle)
+        {
+            // Pooja's Applications
+            string result;
+            if ((firstAngle + secondAngle + thirdAngle) == 180)  //mutation testing(Corrected)
+            {
+                result = "The triangle is valid.";
+            }
+            else
+            {
+                result = "The triangle is NOT valid.";
+            }
+            return result;
+        }
+    }
+}
